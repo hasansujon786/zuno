25c7ba4bb68066ed3d17a426a757e8c32a9fcf70
Hasan Mahmud <38243998+hasansujon786@users.noreply.github.com>
2025-09-16 18:44:04 +0600

    fix: use Link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,21 @@
+import Link from "next/link"
+
 export default function Navbar() {
   return (
     <div className="fixed top-3 z-50 flex w-full justify-center">
       <div className="shaow-sm flex h-14 w-full max-w-3xl items-center justify-between rounded-lg border border-zinc-100 bg-white/90 p-1.5 px-2">
-        <a href="/" aria-label="home" className="ml-3">
+        <Link href="/" aria-label="home" className="ml-3">
           <img
             className="h-4"
             src="https://cdn.prod.website-files.com/675c8e48ca0e0fb5ab421239/67ea1567f801b7bf3d63fad7_zuno-logo-b.svg"
             loading="lazy"
             alt=""
           />
-        </a>
+        </Link>
 
-        <div className="flex justify-center items-center gap-2">
+        <div className="flex items-center justify-center gap-2">
           {["Home", "Pages", "Template"].map((title) => (
-            <a className="hover:bg-black/20 px-2 py-1 rounded-sm" key={title} href="#">
+            <a className="rounded-sm px-2 py-1 hover:bg-black/20" key={title} href="#">
               {title}
             </a>
           ))}
